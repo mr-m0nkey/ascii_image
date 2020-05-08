89e423b5a29d5bd1328fd0c9c0f9da3a39f06d14
Mayowa Aj <sagaraaj69@gmail.com>
2020-05-08 21:15:50 +0100

    stores characters in a variable

--- main.go
+++ main.go
@@ -31,17 +31,18 @@ func main() {
 
   bounds := m.Bounds()
 
-  // asciiLength := 70
-//   TODO refactor 
+  asciiCharacters := "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+  asciiLength := len(asciiCharacters)
+
   for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
     for x := bounds.Min.X; x < bounds.Max.X; x++ {
       r, g, b, _ := m.At(x, y).RGBA()
       total := uint32(r) + uint32(g) + uint32(b)
-      asciiIndex  := uint32(float32(total) / float32(199999) * float32(55))
+      asciiIndex  := uint32(float32(total) / float32(199999) * float32(asciiLength))
       
-      fmt.Print(string("`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"[asciiIndex ]))
-      fmt.Print(string("`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"[asciiIndex ]))
-      fmt.Print(string("`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"[asciiIndex ]))
+      fmt.Print(string(asciiCharacters[asciiIndex ]))
+      fmt.Print(string(asciiCharacters[asciiIndex ]))
+      fmt.Print(string(asciiCharacters[asciiIndex ]))
     }
     fmt.Println("\n")
   }
