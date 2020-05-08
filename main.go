d15eb75ce6c46dc5cbcac853f2870a96e9087c2a
Mayowa Aj <sagaraaj69@gmail.com>
2020-05-08 22:32:10 +0100

    go fmt

--- main.go
+++ main.go
@@ -1,54 +1,51 @@
 package main
 
-
 import (
-  "fmt"
-  "image"
-  "os"
-  "log"
-  _ "image/gif"
-  _ "image/png"
-  _ "image/jpeg"
+	"fmt"
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+	"log"
+	"os"
 )
 
-
 func main() {
-  
-  image_name := os.Args[1]
 
-  // TODO throw error if argument is missing
-  
-  reader, err := os.Open(image_name)
-  
-  if err != nil {
-      log.Fatal(err)
-  }
-  defer reader.Close()
+	imageName := os.Args[1]
 
-  m, _, err := image.Decode(reader)
-  if err != nil {
-    log.Fatal(err)
-  }
+	// TODO throw error if argument is missing
 
-//   TODO resize image so it fits into the terminal
+	reader, err := os.Open(imageName)
 
-  bounds := m.Bounds()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer reader.Close()
 
-  asciiCharacters := "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
-  asciiLength := len(asciiCharacters)
+	m, _, err := image.Decode(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-    for x := bounds.Min.X; x < bounds.Max.X; x++ {
-      r, g, b, _ := m.At(x, y).RGBA()
-      total := uint32(r) + uint32(g) + uint32(b)
-      asciiIndex  := uint32(float32(total) / float32(199999) * float32(asciiLength))
-      
-      for thickness := 0; thickness < 3; thickness++ {
-        fmt.Print(string(asciiCharacters[asciiIndex ]))
-      }
-    }
-    fmt.Println("\n")
-  }
+	//   TODO resize image so it fits into the terminal
 
+	bounds := m.Bounds()
+
+	asciiCharacters := "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+	asciiLength := len(asciiCharacters)
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r, g, b, _ := m.At(x, y).RGBA()
+			total := uint32(r) + uint32(g) + uint32(b)
+			asciiIndex := uint32(float32(total) / float32(199999) * float32(asciiLength))
+
+			for thickness := 0; thickness < 3; thickness++ {
+				fmt.Print(string(asciiCharacters[asciiIndex]))
+			}
+		}
+		fmt.Println("\n")
+	}
 
 }
